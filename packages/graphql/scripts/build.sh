cded44e80dc657cba9b1f5be5e98e605fda24c03
John Yuan <john.ycq@outlook.com>
2023-09-18 15:19:18 +0800

    chore: update package settings and generate coverage badges

--- packages/graphql/scripts/build.sh
+++ packages/graphql/scripts/build.sh
@@ -8,6 +8,7 @@ rm -rf es
 rm -rf lib
 
 npm run test
+npm run coverage:badges
 npm run build
 npm run lint:fix
 npm run format
@@ -15,5 +16,8 @@ npm run format
 rm es/index.test.*
 rm lib/index.test.*
 rm lib/index.d.ts
+
+rm -rf es/example
+rm -rf lib/example
 
 npm run minify
