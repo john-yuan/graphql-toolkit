6df0febdd177262a65dd0da5b23c2273ec9e0bb2
John Yuan <john.ycq@outlook.com>
2025-02-05 14:35:55 +0800

    feat: export convertSchema

--- packages/generate-graphql-client/src/index.ts
+++ packages/generate-graphql-client/src/index.ts
@@ -1,3 +1,17 @@
 export { generate } from './core/generate'
+export { convertSchema } from './core/convertSchema'
 export type { GenerateOptions } from './core/generate'
-export type { Configuration, SchemaFile, Options, Endpoint } from './core/types'
+export type {
+  Configuration,
+  SchemaFile,
+  Options,
+  Endpoint,
+  Schema,
+  Directive,
+  DirectiveLocation,
+  EnumValue,
+  Field,
+  InputValue,
+  Type,
+  TypeKind
+} from './core/types'
