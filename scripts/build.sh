d69b147ae65082cd7be996529d4da648a7387f69
John Yuan <john.ycq@outlook.com>
2025-02-13 17:24:16 +0800

    build: add build script

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+readonly ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
+
+cd $ROOT_DIR
+cd packages/generate-graphql-client
+
+./scripts/build.sh
+
+cd $ROOT_DIR
+cd packages/generate-graphql-query
+
+./scripts/build.sh
+
+cd $ROOT_DIR
+cd packages/generate-graphql-introspection
+
+./scripts/build.sh
