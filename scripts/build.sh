099512a985b732cb3b5bbd96296162d84585e7de
John Yuan <john.ycq@outlook.com>
2026-01-23 09:40:55 +0800

    build: fix build.sh and add prepare script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,16 @@
 readonly ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
 
 cd $ROOT_DIR
+cd packages/generate-graphql-introspection
+
+./scripts/build.sh
+
+cd $ROOT_DIR
 cd packages/generate-graphql-client
 
 ./scripts/build.sh
 
 cd $ROOT_DIR
 cd packages/generate-graphql-query
-
-./scripts/build.sh
-
-cd $ROOT_DIR
-cd packages/generate-graphql-introspection
 
 ./scripts/build.sh
